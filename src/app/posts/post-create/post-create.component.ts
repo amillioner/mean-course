19ee1878af5a843a1d6fdb4a62e0e45a1b6abef3
AnwarAkhror <anwar.akhror@gmail.com>
2019-04-05 10:37:35 -0400

    commit2

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
-  styleUrls: ['/post-create.component.css'],
+  styleUrls: ["/post-create.component.css"]
 })
 export class PostCreateComponent {
-  enteredValue = "";
-  newPost = "No Content";
+  enteredTitle = "";
+  enteredContent = "";
+  @Output() postCreated = new EventEmitter();
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 
+  // enteredValue = "";
+  // newPost = "No Content";
+  // onAddPost() {
+  // this.newPost = this.enteredValue;
+  // }
   //listen to the methon on click event
   // onAddPost(postInput: HTMLTextAreaElement) {
   //   // console.dir(postInput);
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: "apop-post-list",
+  selector: "app-post-list",
   templateUrl: "./post-list.component.html",
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent {
-  posts=[
-    {title: 'First Post', content: {'This is the first post\'s content'},
-    {title: 'Second Post', content: {'This is the second post\'s content'},
-    {title: 'Third Post', content: {'This is the third post\'s content'},
-  ];
+  @Input() posts = [];
+    // posts = [
+  //   {title: "First Post", content: "This is the first post\'s content" },
+  //   {title: "Second Post", content: "This is the second post\'s content" },
+  //   {title: "Third Post", content: "This is the third post\'s content" },
+  // ];
 }
